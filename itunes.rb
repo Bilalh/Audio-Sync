ee0c99dc98ed41dc96cb0e20c36ae5550be20a99
Bilal Hussain <bhterra@gmail.com>
2010-09-01 23:49:42 +0000

    git-svn-id: file://localhost/Users/bilalh/Programming/svn/Ruby/svn/Ruby/branches/itunes_sync@193 7eb1ea8d-9fb6-4085-bc48-dd495116b08e

--- itunes.rb
+++ itunes.rb
@@ -39,24 +39,33 @@ module Sync
 		def add_album(name)
 			val = self[name]
 			return val if val
-			return (self[name] = Set.new)
+			return (self[name] = Music.new)
 		end
-
+		
+		def subtract(hash)
+			hash.each do |o|
+				self.delete(o[0])
+			end
+		end
+		
+		def add_music!(hash)
+			hash.each do |o|
+				self[o[0]] = o.dup
+			end
+		end
+		
 		# remove the elements from the other inplace
 		def minus!(other)
-			
 			other.each_pair do |x, o_arist|
-				if artist = self[x] then
+				if  artist = self[x] then
 					o_arist.each_pair do |y, o_album|
 						if album = artist[y] then
 							album.subtract o_album
 						end
 						artist.delete y if album.size == 0
-						
 					end
+					self.delete x if artist.size == 0
 				end
-				self.delete x if artist.size == 0
-				
 			end	
 		end
 		
@@ -66,8 +75,25 @@ module Sync
 				artist.each do |name, album|
 					puts "  album: #{name}"
 					album.each do |track|
-						puts "     #{track}"
+						puts "     #{track[0]}"
 					end
+				end
+			end
+		end
+	
+		def merge_music(other)
+			other.each_pair do |x, o_arist|
+				if  (artist = self[x]) then
+					o_arist.each_pair do |y, o_album|
+						if album = artist[y] then
+							album.add_music! o_album
+						else 
+							artist[y] = o_album.dup
+						end
+					end
+					
+				else
+					self[x] = o_arist.dup
 				end
 			end
 		end
@@ -77,41 +103,54 @@ module Sync
 	class Itunes
 		attr_reader :app, :music, :playlists, :synced
 		
+		def print_musica(hash)
+			hash.each_pair do |name, artist|
+				puts "artist: #{name}"
+				artist.each do |name, album|
+					puts "  album: #{name}"
+					album.each do |track|
+						puts "     #{track[0]}"
+					end
+				end
+			end
+		end
+		
 		def initialize(base = File.expand_path("~/Desktop/music_t") )
 			@app       = get_app 'com.apple.itunes','itunes'
-			@music     = Music.new
 			@base      = base
+			@music     = Music.new
 			@playlists = @app.sources.first.playlists
 			@synced    = Music.new
+			@m3u       = Hash.new
+			@file      = NSFileManager.new
 		end
 		
 		# Makes the playlist with name
 		def make_playlist_data(name)
+			
+			ply = "#EXTM3U\n"
 			@playlists[name].tracks.each do |track|
 				url   = track.get.location
 				parts = url.pathComponents
-				name = parts[-1]; album = parts[-2]; artist = parts[-3]
-				
+				name  = parts[-1]; album = parts[-2]; artist = parts[-3]
+				full   = "#{artist}/#{album}/#{name}"
+
 				@music.add_artist(artist)
 					.add_album(album)
-					.add(name)
+					.store(name,full)
+
+				ply << "#EXTINF:-1,#{track.name} - #{artist}\n"
+				ply << "../#{full}\n"
 			end
+			
+			@m3u[name] = ply
 			return self
 		end
-	
-		def sync
-			file = NSFileManager.new
-			# file.copyItemAtPath(
-			# 	url.path,
-			# 	toPath:path,
-			# 	error:error
-			# )
-			return self
-		end
-		
+
 		def save_synced(path="#{@base}/sync.yaml")
+			synced =  @synced.size == 0 ? @music : @synced
 			File.open(path, "w") do |file|
-				file.write(@music.to_yaml)
+				file.write(synced.to_yaml)
 			end
 			return self
 		end
@@ -122,13 +161,73 @@ module Sync
 		end
 		
 		def find_unsyced(synced = @synced)
+			
+			puts "osynced" 
+			print_musica synced
+			puts
+			
+			puts "music" 
+			print_musica  @music
+			puts
+			temp = @music.dup.merge_music synced
+
 			@music.minus! synced
+			@synced = temp
+			puts "synced" 
+			print_musica @synced
+			puts
+			
+			puts "new" 
+			print_musica  @music
+			return self
+		end
+		
+		def write_unsynced
+			Dir.chdir(@base)
+			@music.each_pair do |ar, artist|
+				mkdir(ar)
+				artist.each_pair do |al, album|
+					mkdir("#{ar}/#{al}")
+					album.each do |track|
+						puts @file.copyItemAtPath(
+							"/Users/bilalh/Music/iTunes/iTunes Music/" + track[1],
+							toPath:track[1],
+							error:nil
+						)
+					end	
+				end
+			end
+			
+			return self
+		end
+	
+		def make_m3u()
+			ply = "#EXTM3U\n"
+			@music.each_pair do |ar, artist|
+				artist.each_pair do |al, album|
+					album.each do |track|
+						ply << "#EXTINF:-1,#{track} - #{ar}\n"
+						ply << "../#{ar}/#{al}/#{track}\n"
+					end
+				end
+			end
+			@m3u["Ωnew"] = ply
 			return self
 		end
 		
 		def print_music(music = @music)
 			music.print_music()
 			return self
+		end
+		
+		private
+		def mkdir(dir)
+			@file.createDirectoryAtPath(
+				dir,
+				withIntermediateDirectories:false,
+				attributes:nil,
+				error:nil
+			)
 		end
 		
 	end
@@ -140,9 +239,11 @@ end
 include Sync 
 
 itunes = Itunes.new
-itunes.make_playlist_data "L5"
-itunes.load_synced()
-itunes.find_unsyced()
-itunes.print_music
-
-
+itunes.make_playlist_data "ar"
+itunes.load_synced
+itunes.find_unsyced
+# # itunes.make_m3u
+# itunes.print_music
+# itunes.print_music
+# # itunes.write_unsynced
+# itunes.save_synced
