8779e7bb307c1e09d590be38bfbbf183ad73842e
Bilal Hussain <bhterra@gmail.com>
2010-09-03 21:20:15 +0000

    delete find
    git-svn-id: file://localhost/Users/bilalh/Programming/svn/Ruby/svn/Ruby/branches/itunes_sync@196 7eb1ea8d-9fb6-4085-bc48-dd495116b08e

--- itunes.rb
+++ itunes.rb
@@ -1,4 +1,5 @@
 #!/usr/local/bin/macruby
+# encoding: utf-8
 framework 'Cocoa'
 framework 'ScriptingBridge'
 
@@ -44,12 +45,6 @@ module Sync
 		def subtract(hash)
 			hash.each do |o|
 				self.delete(o[0])
-			end
-		end
-		
-		def add_music!(hash)
-			hash.each do |o|
-				self[o[0]] = o.dup
 			end
 		end
 		
@@ -109,20 +104,30 @@ module Sync
 	end
 	
 	class Itunes
-		attr_reader :app, :music, :playlists, :synced
+		attr_reader :app,:delete, :music, :playlists, :pnames, :synced
 
 		def initialize(base = File.expand_path("~/Desktop/music_t") )
 			@app       = get_app 'com.apple.itunes','itunes'
 			@base      = base
+			@file      = NSFileManager.new
+			@m3u       = Hash.new
 			@music     = Music.new
 			@playlists = @app.sources.first.playlists
 			@synced    = Music.new
-			@m3u       = Hash.new
-			@file      = NSFileManager.new
+			@delete    = []
+			
+			@pnames    = []
+			@psize     = {}
+			@playlists.each do |p|
+				name          = p.name
+				@pnames      << name
+				@psize[name]  = p.size
+			end
 		end
 		
 		# Makes the playlist with name
 		def make_playlist_data(p_name)
+			raise NoPlayList unless @pnames.include? p_name
 			
 			ply = "#EXTM3U\n"
 			@playlists[p_name].tracks.each do |track|
@@ -144,7 +149,12 @@ module Sync
 		end
 
 		def load_synced(path="#{@base}/sync.yaml")
-			@synced =  File.open(path){|y| YAML.load(y) }
+			@synced = 
+				if File.exist?(path)   then
+					File.open(path){|y| YAML.load(y) }
+				else
+					 Music.new
+				end
 			return self
 		end
 		
@@ -165,6 +175,38 @@ module Sync
 			print_music  @music
 			puts
 			
+			return self
+		end
+		
+		# find all songs that are not in any playlist
+		def find_not_in_playlist
+			@synced.each_pair do |ar, artist|
+				
+				if ( m_ar = @music[ar] ) then
+					artist.each_pair do |al, album|
+						if ( m_al = m_ar[al] ) then
+							album.each do |name, full|
+								delete << full unless m_al[name]
+							end
+						else 
+							puts "album  #{al} not found"
+							album.each do |name, full|
+								@delete << full
+							end
+						end
+					end
+					
+				else 
+					puts "artist #{ar} not found"
+					artist.each_pair do |al, album|
+						album.each_pair do |name, full|
+							@delete << full
+						end
+					end
+				end
+				
+			end
+
 			return self
 		end
 		
@@ -194,8 +236,14 @@ module Sync
 			end
 			return self
 		end
-
-		def make_m3u
+		
+		
+		def delete_not_found
+		
+		end
+		
+		def make_new_playlist
+			return if @music.size == 0 
 			ply = "#EXTM3U\n"
 			@music.each_pair do |ar, artist|
 				artist.each_pair do |al, album|
@@ -254,12 +302,14 @@ include Sync
 
 itunes = Itunes.new
 itunes.make_playlist_data "pc [701,2500]"
-itunes.make_playlist_data "↑"
+# itunes.make_playlist_data "↑ "
 itunes.load_synced
-itunes.find_unsyced
-# itunes.print_music
-itunes.write_unsynced
-itunes.save_synced
-itunes.make_m3u
-itunes.save_m3us
+itunes.find_not_in_playlist
+
+# itunes.find_unsyced
+# itunes.write_unsynced
+# itunes.save_synced
+# itunes.make_new_playlist
+# itunes.save_m3us
+
 
